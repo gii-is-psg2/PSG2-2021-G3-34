4226093748d45d0151b54bc6cb8d06119632765d
Isabel <isadede@alum.us.es>
2021-03-17 17:55:55 +0100

    Traducción welcome y ownerController

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -39,6 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/admin/**").hasAnyAuthority("admin")
 				.antMatchers("/owners/**").hasAnyAuthority("owner","admin")				
 				.antMatchers("/vets/**").authenticated()
+				.antMatchers("/bienvenido").permitAll()
+				.antMatchers("/welcome").permitAll()
 				.anyRequest().denyAll()
 				.and()
 				 	.formLogin()
