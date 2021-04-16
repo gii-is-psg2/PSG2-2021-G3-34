7d40a72b4e466c57ca9714957f28a3dfbc3a4e82
Isabel <isadede@alum.us.es>
2021-04-16 09:00:22 +0200

    A3.2.Causes.Model #60

--- src/main/java/org/springframework/samples/petclinic/model/Cause.java
+++ src/main/java/org/springframework/samples/petclinic/model/Cause.java
@@ -1,5 +1,9 @@
 package org.springframework.samples.petclinic.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -7,8 +11,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,12 +34,35 @@ public class Cause extends NamedEntity {
 	
 	@Column(name="target")
 	@NotNull
-	private Integer target;
+	@Min(0)
+	private Integer budgetTarget;
 	
 	@Column(name="organisation")
 	@NotBlank
 	private String organisation;
+
+	@NotNull
+	@Column(name = "is_closed")
+	private Boolean isClosed;
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "cause")
 	private Set<Donation> donations;
+	
+	protected Set<Donation> getDonationsInternal() {
+	    if (this.donations == null) {
+	       this.donations = new HashSet<>();
+	    }
+	    return this.donations;
+	}
+
+	public List<Donation> getDonations() {
+	    List<Donation> sortedDonations = new ArrayList<>(getDonationsInternal());
+	    PropertyComparator.sort(sortedDonations, new MutableSortDefinition("date", false, false));
+	    return Collections.unmodifiableList(sortedDonations);
+	}
+
+	public void addDonation(Donation donation) {
+	    getDonationsInternal().add(donation);
+	    donation.setCause(this);
+	}
 }
