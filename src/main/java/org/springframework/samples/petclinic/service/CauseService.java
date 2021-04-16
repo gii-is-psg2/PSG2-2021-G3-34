60690d6c01fac9d624fb5a55c711f73c8359ffe8
Isabel <isadede@alum.us.es>
2021-04-16 10:17:58 +0200

    A3.2.Causes.Service

---
+++ src/main/java/org/springframework/samples/petclinic/service/CauseService.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Cause;
+import org.springframework.samples.petclinic.repository.CauseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CauseService {
+	
+	private final CauseRepository causeRepository;
+	
+	@Autowired
+	public CauseService(CauseRepository causeRepository) {
+		this.causeRepository = causeRepository;
+	}
+
+	@Transactional
+	public void saveCause(Cause cause) throws DataAccessException {
+		causeRepository.save(cause);
+	}
+	
+	public Cause findCauseById(int causeId) {
+		return causeRepository.findByCauseId(causeId);
+	}
+	
+
+	@Transactional
+	public Collection<Cause> findCauses()  {
+		return causeRepository.findAll();
+	}
+}
