ef97a45e79e6501a6808fbeabbc8d8d23ed4b4ff
Isabel <isadede@alum.us.es>
2021-04-16 09:05:15 +0200

    A3.2.Causes.Repository

--- src/main/java/org/springframework/samples/petclinic/repository/CauseRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/CauseRepository.java
@@ -3,7 +3,9 @@ package org.springframework.samples.petclinic.repository;
 import java.util.Collection;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Cause;
 
 public interface CauseRepository extends Repository<Cause, Integer>{
@@ -11,4 +13,13 @@ public interface CauseRepository extends Repository<Cause, Integer>{
 	Collection<Cause> findAll() throws DataAccessException;
 	
 	void save(Cause cause) throws DataAccessException;
+	
+	@Query("SELECT c FROM Cause c where c.id=:causeId")
+	Cause findByCauseId(@Param(value = "causeId") int causeId);
+		
+	@Query("SELECT sum(d.amount) FROM Donation d where d.cause.id=:causeId")
+	Double totalBudget(@Param(value = "causeId") int causeId);
+	    
+	@Query("SELECT d FROM Donation d where d.cause.id=:causeId")
+	Collection<Donation> findDonations(@Param(value = "causeId") int causeId);
 }
--- src/main/java/org/springframework/samples/petclinic/repository/DonationRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/DonationRepository.java
@@ -3,7 +3,9 @@ package org.springframework.samples.petclinic.repository;
 import java.util.Collection;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Donation;
 
 public interface DonationRepository extends Repository<Donation, Integer>{
@@ -11,4 +13,10 @@ public interface DonationRepository extends Repository<Donation, Integer>{
 	Collection<Donation> findAll() throws DataAccessException;
 	
 	void save(Donation donation) throws DataAccessException;
+	
+	@Query("SELECT d FROM Donation d where d.id=:donationId")
+	Donation findByDonationId(@Param(value = "donationId") int donationId);
+	
+	@Query("SELECT d FROM Donation d where d.cause.id=:causeId")
+	Collection<Donation> findByCauseId(@Param(value = "causeId") int causeId);
 }
